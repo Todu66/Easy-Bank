9fa5c13b33a6aca2e99df6adea36a32155731c98
Todu66 <giga.todua06@gmail.com>
2022-08-21 12:54:53 -0700

    commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+const liEl = document.querySelector(".main-nav-list, .main-nav-link");
+
+btnNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+});
+
+liEl.addEventListener("click", function () {
+  headerEl.classList.remove("nav-open")
+});
+
